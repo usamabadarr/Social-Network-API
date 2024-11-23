6a9f32206ceff7d1a443d0df24d5d699d6fa2666
usamabadarr <usamabadarr@gmail.com>
2024-11-23 14:23:23 -0500

    added .env file and updated db

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -1,15 +1,20 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const db = async (): Promise<typeof mongoose.connection> =>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmediaDB');
-        console.log('Database connected.');
-        return mongoose.connection;
-    } catch(error) {
-        console.log(error);
-        console.error('Database connection error:', error);
-        throw new Error('Database connection failed.');
-    }
-}
+// Load environment variables from the .env file
+dotenv.config();
+
+const db = async (): Promise<typeof mongoose.connection> => {
+  try {
+    // Use the MONGO_URI from the environment variables
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/social_network_db');
+    console.log('Database connected.');
+    return mongoose.connection;
+  } catch (error) {
+    console.log(error);
+    console.error('Database connection error:', error);
+    throw new Error('Database connection failed.');
+  }
+};
 
 export default db;
